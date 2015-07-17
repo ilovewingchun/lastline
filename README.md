1f3d22465ad8ddeda766bc31a852e92748d1b983
Tyler <alphaone.tw@gmail.com>
2015-07-17 18:18:19 +0800

    New version of Python script
    Now we use a single Python script to finish the job.

--- README.md
+++ README.md
@@ -1,26 +1,10 @@
-1. Use "get_events.sh" to download events from Lastline Manager and save it to "events.json". Be sure to read the code itself and modify several parameters before using it. You can change the time window of how long you want your latest detected events being exported out from Manager.
+This is a Python script that will help you to:
 
-2. The "extract_ip.py" is used to parse downloaded event file and extract ip information out and save it to another file default to "block_ip.txt". The "extract_ip.py" will look for entries in "whitelist.txt" file and not write them to "block_ip.txt", so put your white list entries in "whitelist.txt" file by yourself. Be sure you have "whitelist.txt" file in place or this script will fail to run.
-3. "extract_dns.py" is trying to do the exact thing as "extract_ip.py" does. The difference between them is this one only extract bad domain names from "events.json" and save it to "block_dns.txt".
-4. Make a shell script to first run "get_events.sh" then "extract_ip.py". Put this script in cron table so it can work by schedule. For example you should have something like this in your /etc/crontab file:
+1. Login to Lastline Manager web portal.
+2. Download events that is Command&Control and with infection level priority.
+3. Parse that events and extract bad destination IP addresses and output to a file(default to block_ip.txt)
 
-45 * * * * root sh /home/tyler/lastline.sh
+By using this script, you will be able to automatically complete the above mentioned workflow. You can then publish that bad IP list and let products such as PaloAlto NGFW or Bluecoat ProxySG to come and get the list. So those inlince security devices and import and do blocking automatically.
 
-And the content of lastline.sh should look something like this:
-
-#!/bin/bash
-/bin/bash /root/get_events.sh
-/usr/bin/python /root/extract_ip.py -o /var/www/block_ip.txt
-exit 0
-
-Notice you can use -o option to tell extract_ip.py to save output file to another location.
-
-5. Use a Apache server to host your latest "block_ip.txt" or "block_dns.txt" and let PaloAlto to retrieve it using Dynamic Block List. The same idea applies to Bluecoat SG Proxy.
-
-All my scripts and my files are look like this:
-
-shell> ls -al /root/get_events.sh /root/extract_ip.py /root/events.json /var/www/block_ip.txt 
--rw-r--r-- 1 root root 592725 Nov  4 10:45 /root/events.json
--rwxr-xr-x 1 root root   3171 Mar 25  2014 /root/extract_ip.py
--rwxr-xr-x 1 root root   1849 Mar 25  2014 /root/get_events.sh
--rw-r--r-- 1 root root    128 Nov  4 10:45 /var/www/block_ip.txt
+For bugs and feedback please report back to me:
+alphaone.tw@gmail.com or tyler@lastline.com
---
+++ get_events.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from datetime import datetime, timedelta
+try:
+	import requests
+except ImportError:
+	print "Please install requests python module\nhttp://www.python-requests.org/en/latest/user/install/#install"
+	sys.exit()
+import json
+import csv
+import argparse
+from pprint import pprint
+import sys
+import os
+import getpass
+# Is there argparse to use?
+try:
+    import argparse
+except ImportError:
+    print "[-] Error! Please install the argparse python module\non Debian systems you can use:\napt-get install python-argparse"
+    sys.exit()
+#http://stackoverflow.com/questions/27921629/python-using-getpass-with-argparse
+class PasswordPromptAction(argparse.Action):
+    def __init__(self,
+             option_strings,
+             dest=None,
+             nargs=0,
+             default=None,
+             required=False,
+             type=None,
+             metavar=None,
+             help=None):
+        super(PasswordPromptAction, self).__init__(
+             option_strings=option_strings,
+             dest=dest,
+             nargs=nargs,
+             default=default,
+             required=required,
+             metavar=metavar,
+             type=type,
+             help=help)
+
+    def __call__(self, parser, args, values, option_string=None):
+        password = getpass.getpass()
+        setattr(args, self.dest, password)
+parser = argparse.ArgumentParser(
+                                 description = "This is a tool to extract IP addresses from an Lastline Enterprise exported event file in JSON format.",     # text displayed on top of --help
+                                 epilog = 'Use it at your own risk!') # last text displayed
+parser.add_argument('-o','--output_file',action="store",default='block_ip.txt',dest='out_file',help='List of extracted bad remote IP addresses, default to "block_ip.txt"')
+parser.add_argument('-wl','--whitelist_file',action="store",default='whitelist.txt',dest='whitelist_file',help='If you want to whitelist certain bad remote IP, put them into a file and point the script to read. This file default to "whitelist.txt"')
+parser.add_argument('-u','--username',dest='username', type=str, required=True, help='Please enter your Lastline portal username.')
+parser.add_argument('-p','--password',dest='password', action=PasswordPromptAction, type=str, required=True, help='Please enter your Lastline portal password.')
+parser.add_argument('-host','--lastline_host',action="store",default='user.lastline.com',dest='lastline_host',help='Lastline Manager host(IP/FQDN). Default to "user.lastline.com.' )
+parser.add_argument('-k','--key-id',action="store",type=str, required=True, dest='key_id',help='License key id. Please check it in Manager web portal in exported event url.' )
+parser.add_argument('-sk','--sub-key-id',action="store",type=str, dest='subkey_id',help='Sensor sub key id. Please check it in Manager web portal in exported event url.' )
+
+arguments = parser.parse_args()
+timenow = datetime.today()
+last1HourDateTime = datetime.today() - timedelta(hours = 1)
+last8HourDateTime = datetime.today() - timedelta(hours = 8)
+last24HourDateTime = datetime.today() - timedelta(hours = 24)
+last7DaysDateTime = datetime.today() - timedelta(days = 7)
+last31DaysDateTime = datetime.today() - timedelta(days = 31)
+
+lastline_host = arguments.lastline_host
+key_id = arguments.key_id
+subkey_id = arguments.subkey_id
+llusername = arguments.username
+llpassword = arguments.password
+
+lastline_url = "https://%s/ll_api/ll_api.php" % lastline_host
+post_data_auth = {'func' : 'is_authenticated', 'username':llusername, 'password':llpassword}
+params_get_events = {'func' : 'events', 'start_datetime':last7DaysDateTime.strftime('%Y-%m-%d+%H:%M:%S'), 'end_datetime':timenow.strftime('%Y-%m-%d+%H:%M:%S'), 'key_id':key_id, 'priority':'Infections', 'threat_class':'command%26control','time_zone':'Asia/Taipei', 'whitelisting':'true', 'show_false_positives':'false', 'format':'json'}
+if subkey_id:
+	params_get_events['subkey_id'] = subkey_id
+string_params = ''.join(['%s=%s&' % (k,v) for k,v in params_get_events.iteritems()])
+req_auth = requests.post(lastline_url, data = post_data_auth)
+req_get_events = requests.get(lastline_url, params = str(string_params), cookies = req_auth.cookies)
+# look at dest in the parser.add_argument lines
+out_file = arguments.out_file
+whitelist_file = arguments.whitelist_file
+if os.path.exists('whitelist'):
+	wl = open('whitelist.txt', 'r').read().splitlines() # Open white list file and remove newline(\n) within it.
+data = json.loads(req_get_events.content) # Load json file and change it to dictionary, store in a variable called data.
+try:
+    a = data["data"] # Retrieve value for key called "data" inside variable data, store in a, this is a list.
+except KeyError:
+    print "[-] Error! No data available!\nPlease input the correct core Lastline values in the beginning of this script!"
+    sys.exit()
+fo = open(out_file, 'w') # Open a file to store our parsed result.
+c = []  # Empty list
+for i in range(len(a)): # Iterate over first level list
+    b = a[i]["dst_host"] # Iterate retrieve IP value for key "dst_host" inside list a
+    c.append(b) # Write each IP into our emtpy list c
+    if os.path.exists('whitelist'):
+    	c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+    else:
+    	d = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in d.
+for item in range(len(d)): # Iterate over our list d.
+    e = d[item] # Store each elements inside a new variable e
+    w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
+    w.writerow([e]) # Write each IP from e to destination file.
+fo.close()
