f2658f9ee1e8aeb6911ac4e85d15aac15a26893e
Tyler <alphaone.tw@gmail.com>
2015-09-22 21:01:34 +0800

    fixing bugs

--- get_events.py
+++ get_events.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# Author: Tyler Chen
+# Contact: alphaone.tw@gmail.com or tyler@lastline.com
 from datetime import datetime, timedelta
 try:
 	import requests
@@ -19,38 +21,13 @@ try:
 except ImportError:
     print "[-] Error! Please install the argparse python module\non Debian systems you can use:\napt-get install python-argparse"
     sys.exit()
-##http://stackoverflow.com/questions/27921629/python-using-getpass-with-argparse
-#class PasswordPromptAction(argparse.Action):
-#    def __init__(self,
-#             option_strings,
-#             dest=None,
-#             nargs=0,
-#             default=None,
-#             required=True,
-#             type=None,
-#             metavar=None,
-#             help=None):
-#        super(PasswordPromptAction, self).__init__(
-#             option_strings=option_strings,
-#             dest=dest,
-#             nargs=nargs,
-#             default=default,
-#             required=required,
-#             metavar=metavar,
-#             type=type,
-#             help=help)
-#
-#    def __call__(self, parser, args, values, option_string=None):
-#        password = getpass.getpass()
-#        setattr(args, self.dest, password)
 
 parser = argparse.ArgumentParser(
                                  description = "This is a tool to extract IP addresses or domain names from an Lastline Enterprise exported event file in JSON format.",     # text displayed on top of --help
                                  epilog = 'Lastline does not support this script! Use it at your own risk!') # last text displayed
 parser.add_argument('-o','--output_file',action="store",default='block.txt',dest='out_file',help='Optional. List of extracted bad remote IP addresses, default to "block.txt"')
-parser.add_argument('-wl','--whitelist_file',action="store",default='whitelist.txt',dest='whitelist_file',help='Optional. If you want to whitelist certain bad remote IP, put them into a file and point the script to read. This file default to "whitelist.txt"')
+parser.add_argument('-wl','--whitelist_file',action="store",dest='whitelist_file',help='Optional. If you want to whitelist certain bad remote IP, put them into a file and point the script to read. This file default to "whitelist.txt"')
 parser.add_argument('-u','--username',dest='username', type=str, required=True, help='Required. Please enter your Lastline portal username.')
-#parser.add_argument('-p','--password',dest='password', action=PasswordPromptAction, type=str, required=False, help='The script will prompt for your password.')
 parser.add_argument('-host','--lastline_host',action="store",default='user.lastline.com',dest='lastline_host',help='Optional. Lastline Manager host(IP/FQDN). Default to "user.lastline.com.' )
 parser.add_argument('-k','--key-id',action="store",type=str, required=True, dest='key_id',help='Required. Lastline Sensor licnese key id(not license key). Please click on </> button on WEB GUI to get this id. (E.g., 123456789)' )
 parser.add_argument('-sk','--sub-key-id',action="store",type=str, dest='subkey_id',help='Optional. Sensor sub key id. Please check it in Manager web portal in exported event url.' )
@@ -59,9 +36,13 @@ parser.add_argument('-m','--method',action="store", dest='method', type=str, req
 parser.add_argument('-tz','--timezone',action="store", dest='timezone', type=str, required=False, default='Asia/Taipei', help='Optional. Your local timezone. Default to "Asia/Taipei".')
 
 arguments = parser.parse_args()
-timenow = datetime.today()
-lastNDaysDateTime = datetime.today() - timedelta(days = arguments.days_ago)
 
+# Defining time variables
+timerange = arguments.days_ago
+timenow = datetime.today()
+lastNDaysDateTime = datetime.today() - timedelta(days = int(timerange))
+
+# Core Lastline values
 lastline_host = arguments.lastline_host
 key_id = arguments.key_id
 subkey_id = arguments.subkey_id
@@ -76,6 +57,26 @@ params_get_events = {'func' : 'events', 'start_datetime':lastNDaysDateTime.strft
 if subkey_id:
 	params_get_events['subkey_id'] = subkey_id
 string_params = ''.join(['%s=%s&' % (k,v) for k,v in params_get_events.iteritems()])
+
+# Check if user has provided enough core Lastline values:
+if not key_id or not llusername or not llpassword:
+    print "[-] Error! Not enough core Lastline values!"
+    print "[-] Please edit this script and provide the correct core Lastline values!"
+    sys.exit()
+
+if method == "ip":
+    print "[+] Trying to download bad destination 'IP' for past %s days." %(timerange)
+    print "[+] Please wait..."
+    print ""
+elif method == "domain":
+    print "[+] Trying to download bad destination 'Domain names' for past %s days." %(timerange)
+    print "[+] Please wait..."
+    print ""
+else:
+    print "[-] Error! Wrong method."
+    print "[-] Please edit this script and provide the correct core Lastline values!"
+    sys.exit()
+
 # Trying to authenticate itself.
 try:
     req_auth = requests.post(lastline_url, data = post_data_auth)
@@ -91,9 +92,10 @@ except Exception, e:
 # look at dest in the parser.add_argument lines
 out_file = arguments.out_file
 whitelist_file = arguments.whitelist_file
-if os.path.exists(whitelist_file):
+# Check if we have whitelist option definied.
+if whitelist_file:
     try:
-	   wl = open('whitelist.txt', 'r').read().splitlines() # Open white list file and remove newline(\n) within it.
+	   wl = open(whitelist_file, 'r').read().splitlines() # Open white list file and remove newline(\n) within it.
     except Exception, e:
         print "[-] Error = " +str(e)
         sys.exit()
@@ -120,28 +122,30 @@ for i in range(len(a)): # Iterate over first level list
     if method == "ip":
         b = a[i]["dst_host"] # Iterate retrieve IP value for key "dst_host" inside list a
         c.append(b) # Write each IP into our emtpy list c
-        if os.path.exists(whitelist_file):
+        if whitelist_file:
     	   c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+           c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
         else:
-    	   d = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in d.
+    	   c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
     elif method == "domain":
         b = a[i]["hostname"] # Iterate retrieve IP value for key "dst_host" inside list a
         c.append(b) # Write each IP into our emtpy list c
-        if os.path.exists(whitelist_file):
+        if whitelist_file:
            c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+           c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
         else:
-           d = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in d.
+           c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
     else:
         print "[-] Error! Cannot iterate over the data."
         print "[-] Please check the method argument again."
 
-# Remove empty strings in list d
-d = filter(len,d)
+# Remove empty strings in list c
+c = filter(len,c)
 
 print "[+] Successfully extracted data '%s'." %(method)
 print "[+] Now writing data to our file '%s'." %(out_file)
-for item in range(len(d)): # Iterate over our list d.
-    e = d[item] # Store each elements inside a new variable e
+for item in range(len(c)): # Iterate over our list c.
+    e = c[item] # Store each elements inside a new variable e
     w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
     w.writerow([e]) # Write each IP from e to destination file.
 fo.close()
--- get_events_script.py
+++ get_events_script.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# Author: Tyler Chen
+# Contact: alphaone.tw@gmail.com or tyler@lastline.com
 #####
 
 # Core Lastline values
@@ -14,6 +16,7 @@ timerange = "7" # Required. Last N days you want to search for. Normally you wil
 method = "ip" # Required. Download IP or Domain name, please input "ip" or "domain" here.
 out_file = "block.txt" # Required. Save results to this file name.
 whitelist_file = "" # Optional. If you wish the script to NOT save certain results, eg. 8.8.8.8, please put them in this file in new line delimited format.
+timezone = "Asia/Taipei" # Required. Please change it to your local time zone. It has to match with what is on Lastline WEB UI,
 
 ##### Starting our codes from here #####
 from datetime import datetime, timedelta
@@ -38,9 +41,11 @@ if not key_id or not llusername or not llpassword:
 if method == "ip":
     print "[+] Trying to download bad destination IP for past %s days." %(timerange)
     print "[+] Please wait..."
+    print ""
 elif method == "domain":
     print "[+] Trying to download bad destination Domain names for past %s days." %(timerange)
     print "[+] Please wait..."
+    print ""
 else:
     print "[-] Error! Wrong method."
     print "[-] Please edit this script and provide the correct core Lastline values!"
@@ -53,7 +58,7 @@ lastNDaysDateTime = datetime.today() - timedelta(days = int(timerange))
 # Lastline Manager(Hosted or On-premise)
 lastline_url = "https://%s/ll_api/ll_api.php" % lastline_host
 post_data_auth = {'func' : 'is_authenticated', 'username':llusername, 'password':llpassword}
-params_get_events = {'func' : 'events', 'start_datetime':lastNDaysDateTime.strftime('%Y-%m-%d+%H:%M:%S'), 'end_datetime':timenow.strftime('%Y-%m-%d+%H:%M:%S'), 'key_id':key_id, 'priority':'Infections', 'threat_class':'command%26control','time_zone':'Asia/Taipei', 'whitelisting':'true', 'show_false_positives':'false', 'format':'json'}
+params_get_events = {'func' : 'events', 'start_datetime':lastNDaysDateTime.strftime('%Y-%m-%d+%H:%M:%S'), 'end_datetime':timenow.strftime('%Y-%m-%d+%H:%M:%S'), 'key_id':key_id, 'priority':'Infections', 'threat_class':'command%26control','time_zone':timezone, 'whitelisting':'true', 'show_false_positives':'false', 'format':'json'}
 if subkey_id:
     params_get_events['subkey_id'] = subkey_id
 
@@ -97,33 +102,35 @@ if not len(a) > 0:
     print "[-] Hint: Use WEB UI, go to Events, set filter to both 'Priority=Infections' and 'Class=Command&Controls', see if there is anything there."
     sys.exit()
 c = []  # Empty list
-for i in range(len(a)): # Iterate over first level list and generate a list d
+for i in range(len(a)): # Iterate over first level list
     if method == "ip":
         b = a[i]["dst_host"] # Iterate retrieve IP value for key "dst_host" inside list a
         c.append(b) # Write each IP into our emtpy list c
         if whitelist_file:
             c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
         else:
-            d = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in d.
+            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
     elif method == "domain":
         b = a[i]["hostname"] # Iterate retrieve IP value for key "dst_host" inside list a
         c.append(b) # Write each IP into our emtpy list c
         if whitelist_file:
             c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
         else:
-            d = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in d.
+            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
     else:
         print "[-] Error! Cannot iterate over the data."
         print "[-] Please check the method variable again."
 
-# Remove empty strings in list d
-d = filter(len,d)
+# Remove empty strings in list c
+c = filter(len,c)
 
 print "[+] Successfully extracted data '%s'." %(method)
 print "[+] Now writing data to our file '%s'." %(out_file)
 fo = open(out_file, 'w') # Open a file to store our parsed result.
-for item in range(len(d)): # Iterate over our list d.
-    e = d[item] # Store each elements inside a new variable e
+for item in range(len(c)): # Iterate over our list c.
+    e = c[item] # Store each elements inside a new variable e
     w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
     w.writerow([e]) # Write each IP/Domain from e to destination file.
 fo.close()
