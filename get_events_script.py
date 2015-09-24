9e290ac2d8f37538ba60b9b2ac641376292ece15
Tyler <alphaone.tw@gmail.com>
2015-09-24 23:00:45 +0800

    now output 3 files: blacklist.txt, blacklist_ip.txt, blacklist_domain.txt

--- get_events_script.py
+++ get_events_script.py
@@ -6,6 +6,7 @@
 
 # Core Lastline values
 # Please input all the core Lastline values over here:
+print "[+] Initializing..."
 
 lastline_host = "user.lastline.com" # Required. Your on-premise IP/FQDN.
 key_id = "" # Required. Lastline Sensor licnese key id(not license key). Please click on </> button on WEB GUI to get this id. (E.g., 123456789)
@@ -13,8 +14,9 @@ subkey_id = "" # Optional. Lastline Sensor subkey id. Leave it blank if you do n
 llusername = "" # Required. Lastline web portal username in email format. (E.g., your@username)
 llpassword = "" # Required. Lastline web portal password. (E.g., mypassword)
 timerange = "7" # Required. Last N days you want to search for. Normally you will search for past 7 days, so you can put a digit number 7 here.
-method = "ip" # Required. Download IP or Domain name, please input "ip" or "domain" here.
-out_file = "block.txt" # Required. Save results to this file name.
+out_file = "blacklist.txt" # Required. Save results to this file name.
+out_file_ip = "blacklist_ip.txt" # Required. Save results to this file name.
+out_file_domain = "blacklist_domain.txt" # Required. Save results to this file name.
 whitelist_file = "" # Optional. If you wish the script to NOT save certain results, eg. 8.8.8.8, please put them in this file in new line delimited format.
 timezone = "Asia/Taipei" # Required. Please change it to your local time zone. It has to match with what is on Lastline WEB UI,
 
@@ -31,23 +33,11 @@ from pprint import pprint
 import sys
 import os
 import getpass
+import logging
 
 # Check if user has provided enough core Lastline values:
 if not key_id or not llusername or not llpassword:
     print "[-] Error! Not enough core Lastline values!"
-    print "[-] Please edit this script and provide the correct core Lastline values!"
-    sys.exit()
-
-if method == "ip":
-    print "[+] Trying to download bad destination IP for past %s days." %(timerange)
-    print "[+] Please wait..."
-    print ""
-elif method == "domain":
-    print "[+] Trying to download bad destination Domain names for past %s days." %(timerange)
-    print "[+] Please wait..."
-    print ""
-else:
-    print "[-] Error! Wrong method."
     print "[-] Please edit this script and provide the correct core Lastline values!"
     sys.exit()
 
@@ -67,11 +57,13 @@ string_params = ''.join(['%s=%s&' % (k,v) for k,v in params_get_events.iteritems
 # Trying to authenticate itself.
 try:
     req_auth = requests.post(lastline_url, data = post_data_auth)
+    print "[+] Contacting Manager."
 except Exception, e:
     print "[-] Error = " +str(e)
     print "[-] Possible network(or firewall) issues. Please check if you have provided correct Lastline host URL."
     print "[-] Please also check if your host can reach to Lastline host URL."
     sys.exit()
+print "[+] Trying to download events."
 try:
     req_get_events = requests.get(lastline_url, params = str(string_params), cookies = req_auth.cookies)
 except Exception, e:
@@ -91,6 +83,7 @@ except Exception, e:
         print "[-] Possible network(or firewall) issues. Please check if you have provided correct Lastline host URL."
         print "[-] Please also check if your host can reach to Lastline host URL."
         sys.exit()
+print "[+] Parsing data."
 try:
     a = data["data"] # Retrieve value for key called "data" inside variable data, store in a, this is a list.
 except Exception, e:
@@ -102,38 +95,63 @@ if not len(a) > 0:
     print "[-] Hint: Use WEB UI, go to Events, set filter to both 'Priority=Infections' and 'Class=Command&Controls', see if there is anything there."
     sys.exit()
 c = []  # Empty list
+list_ip = []
+list_domain = []
 for i in range(len(a)): # Iterate over first level list
-    if method == "ip":
-        b = a[i]["dst_host"] # Iterate retrieve IP value for key "dst_host" inside list a
-        c.append(b) # Write each IP into our emtpy list c
-        if whitelist_file:
-            c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
-            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
-        else:
-            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
-    elif method == "domain":
-        b = a[i]["hostname"] # Iterate retrieve IP value for key "dst_host" inside list a
-        c.append(b) # Write each IP into our emtpy list c
-        if whitelist_file:
-            c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
-            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
-        else:
-            c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
+    ip = a[i]["dst_host"] # Iterate retrieve IP value for key "dst_host" inside list a
+    c.append(ip) # Write each IP into our emtpy list c
+    list_ip.append(ip)
+    if whitelist_file:
+        c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+        list_ip = [x for x in list_ip if x not in wl]
+        c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
+        list_ip = list(set(list_ip))
     else:
-        print "[-] Error! Cannot iterate over the data."
-        print "[-] Please check the method variable again."
-
-# Remove empty strings in list c
+        c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
+        list_ip = list(set(list_ip))
+    hostname = a[i]["hostname"] # Iterate retrieve IP value for key "dst_host" inside list a
+    c.append(hostname) # Write each IP into our emtpy list c
+    list_domain.append(hostname)
+    if whitelist_file:
+        c = [x for x in c if x not in wl] # Remove those entries that are inside whitelist.
+        list_domain = [x for x in list_domain if x not in wl]
+        c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
+        list_domain = list(set(list_domain))
+    else:
+        c = list(set(c)) # Retrieve each elements inside list c, using set function to remove duplicate entries and store in c.
+        list_domain = list(set(list_domain))
+# Remove empty strings
 c = filter(len,c)
+list_ip = filter(len,list_ip)
+list_domain = filter(len,list_domain)
 
-print "[+] Successfully extracted data '%s'." %(method)
-print "[+] Now writing data to our file '%s'." %(out_file)
+# Sorting
+c.sort()
+list_ip.sort()
+list_domain.sort()
+
+print "[+] Successfully extracted IP/Domain."
+print "[+] Now writing IP/Domain to our file '%s'." %(out_file)
 fo = open(out_file, 'w') # Open a file to store our parsed result.
 for item in range(len(c)): # Iterate over our list c.
     e = c[item] # Store each elements inside a new variable e
     w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
     w.writerow([e]) # Write each IP/Domain from e to destination file.
 fo.close()
-print "[+] Successfully downloaded '%s' list into our file '%s'." %(method, out_file)
+print "[+] Now writing IP to our file '%s'." %(out_file_ip)
+fo = open(out_file_ip, 'w') # Open a file to store our parsed result.
+for item in range(len(list_ip)): # Iterate over our list c.
+    e_ip = list_ip[item] # Store each elements inside a new variable e
+    w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
+    w.writerow([e_ip]) # Write each IP/Domain from e to destination file.
+fo.close()
+print "[+] Now writing Domain to our file '%s'." %(out_file_domain)
+fo = open(out_file_domain, 'w') # Open a file to store our parsed result.
+for item in range(len(list_domain)): # Iterate over our list c.
+    e_domain = list_domain[item] # Store each elements inside a new variable e
+    w = csv.writer(fo, lineterminator="\n") # Using csv function to write each value to a newly definied variable w, which actually writes to previously opened file fo.
+    w.writerow([e_domain]) # Write each IP/Domain from e to destination file.
+fo.close()
+print "[+] Successfully downloaded IP/Domain list into our file '%s'." %(out_file)
 print "[+] Have a nice day!"
 print ""
